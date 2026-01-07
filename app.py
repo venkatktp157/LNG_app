679e880cc5ce84084165a715bad1fb9431313956
venkatktp157 <vatsal157@gmail.com>
2026-01-07 11:35:11 +0530

    modified data files added vessels

--- app.py
+++ app.py
@@ -61,7 +61,8 @@ if auth_status:
         "MOUNT GOWER": ["LNG_TK1", "LNG_TK2"],
         "MOUNT GAEA": ["LNG_TK1", "LNG_TK2"],
         "MOUNT COOK": ["LNG_TK1", "LNG_TK2"],
-        "MOUNT ARARAT": ["LNG_TK1", "LNG_TK2"],    
+        "MOUNT ARARAT": ["LNG_TK1", "LNG_TK2"],  
+        "ATLANTIC PEARL": ["LNG_TK1", "LNG_TK2"],  
         "CMA CGM ARCTIC" : ["LNG_TK"],
         "CMA CGM BALI" : ["LNG_TK"],
         "CMA CGM DIGNITY" : ["LNG_TK"],
@@ -79,6 +80,7 @@ if auth_status:
         "ZIM SCORPIO" : ["LNG_TANK"],
         "QUETZAL" : ["LNGAS_TK"],
         "COPAN" : ["LNGAS_TK"],
+        "TISCAPA": ["LNGAS_TK"],
         "CMA CGM DAYTONA": ["LNG_TK1", "LNG_TK2"],
         "CMA CGM INDIANAPOLIS": ["LNG_TK1", "LNG_TK2"],
         "CMA CGM MONACO": ["LNG_TK1", "LNG_TK2"],
@@ -570,13 +572,19 @@ if auth_status:
             LNG_TK2_cap = 2322.097
             identity = "110k_tanker"
 
+        elif ship_id in ["ATLANTIC PEARL"]:   #111K_tanker
+            BOG_max = 1200    # to be ascertained
+            LNG_TK1_cap = 1816.435
+            LNG_TK2_cap = 1818.006
+            identity = "111k_tanker"    
+
         elif ship_id in ["STARWAY", "GREENWAY"]:   #150K_tanker
             BOG_max = 1200
             LNG_TK1_cap = 2570.133
             LNG_TK2_cap = 2571.517
             identity = "150k_tanker"   
             
-        elif ship_id in ["QUETZAL", "COPAN"]:   #1400TEU_cont
+        elif ship_id in ["QUETZAL", "COPAN","TISCAPA"]:   #1400TEU_cont
             BOG_max = 500
             LNG_TK1_cap = 1613
             identity = "1400TEU_cont"    
